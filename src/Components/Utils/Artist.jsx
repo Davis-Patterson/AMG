7576f118f2401824bd29d3eff095ae5ced5507e4
Davis-Patterson <Davisfpatterson@gmail.com>
2024-06-04 22:37:34 -0400

    skeleton pages for article and artist components for before data becomes available

--- src/Components/Utils/Artist.jsx
+++ src/Components/Utils/Artist.jsx
@@ -1,22 +1,110 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { AppContext } from 'contexts/AppContext';
 import onAirImg from 'assets/News/on-air.jpg';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import CircularProgress from '@mui/material/CircularProgress';
 import 'styles/Artist.css';
 
 function Artist() {
   const { name } = useParams();
   const { artistData, noUserImg, formatTitleForURL } = useContext(AppContext);
+  const [artist, setArtist] = useState(null);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, []);
+    if (artistData.length > 0) {
+      const foundArtist = artistData.find(
+        (artist) => formatTitleForURL(artist.name) === name
+      );
+      setArtist(foundArtist);
+    }
+  }, [artistData, name, formatTitleForURL]);
 
-  const artist = artistData.find(
-    (artist) => formatTitleForURL(artist.name) === name
-  );
-
-  const headerImage = artist.banner || artist.img || noUserImg;
+  if (!artistData || artistData.length === 0) {
+    return (
+      <main className='page-container' id='page-container'>
+        <header className='artists-header' id='artists-header'>
+          <section className='artists-header-pics-container'>
+            <Skeleton className='header-skeleton' />
+            <div className='circular-progress-container'>
+              <CircularProgress
+                size={40}
+                sx={{
+                  color: 'var(--clr-divider)',
+                }}
+              />
+            </div>
+          </section>
+        </header>
+        <section className='loading-content-container'>
+          <div className='loading-content'>
+            <div className='skeleton-article-cards'>
+              <div className='skeleton-article-card'>
+                <div className='skeleton-article-image'>
+                  <Skeleton height={150} width={150} />
+                  <div className='circular-progress-container'>
+                    <CircularProgress
+                      size={40}
+                      sx={{
+                        color: 'var(--clr-divider)',
+                      }}
+                    />
+                  </div>
+                </div>
+                <div className='skeleton-article-content'>
+                  <div className='skeleton-shadow-container'>
+                    <Skeleton
+                      width={300}
+                      height={15}
+                      style={{ marginBottom: 0 }}
+                    />
+                  </div>
+                  <div className='skeleton-shadow-container'>
+                    <Skeleton
+                      width={300}
+                      height={15}
+                      style={{ marginBottom: 0 }}
+                    />
+                  </div>
+                  <div className='skeleton-shadow-container'>
+                    <Skeleton
+                      width={300}
+                      height={15}
+                      style={{ marginBottom: 0 }}
+                    />
+                  </div>
+                  <div className='skeleton-shadow-container'>
+                    <Skeleton
+                      width={300}
+                      height={15}
+                      style={{ marginBottom: 0 }}
+                    />
+                  </div>
+                  <div className='skeleton-shadow-container'>
+                    <Skeleton
+                      width={300}
+                      height={15}
+                      style={{ marginBottom: 0 }}
+                    />
+                  </div>
+                  <div className='skeleton-shadow-container'>
+                    <Skeleton
+                      width={300}
+                      height={15}
+                      style={{ marginBottom: 0 }}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div className='gap' />
+      </main>
+    );
+  }
 
   if (!artist) {
     return (
@@ -44,6 +132,8 @@ function Artist() {
       </main>
     );
   }
+
+  const headerImage = artist.banner || artist.img || noUserImg;
 
   return (
     <main className='page-container' id='page-container'>
