f694f3b67c36d628a4d5161a3f38245055286a8d
Davis-Patterson <Davisfpatterson@gmail.com>
2024-06-27 13:08:30 -0400

    prevents scrolling when art is open

--- src/Components/Utils/Artwork.jsx
+++ src/Components/Utils/Artwork.jsx
@@ -1,9 +1,40 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AppContext } from 'contexts/AppContext';
 import 'styles/Utils/Artwork.css';
 
 function Artwork() {
   const { artworkSrc, artworkAlt, artworkClose } = useContext(AppContext);
+
+  useEffect(() => {
+    const preventScroll = (e) => {
+      e.preventDefault();
+    };
+
+    window.addEventListener('wheel', preventScroll, { passive: false });
+    window.addEventListener(
+      'keydown',
+      (e) => {
+        if (
+          [
+            'ArrowUp',
+            'ArrowDown',
+            'PageUp',
+            'PageDown',
+            'Home',
+            'End',
+          ].includes(e.key)
+        ) {
+          preventScroll(e);
+        }
+      },
+      { passive: false }
+    );
+
+    return () => {
+      window.removeEventListener('wheel', preventScroll, { passive: false });
+      window.removeEventListener('keydown', preventScroll, { passive: false });
+    };
+  }, []);
 
   return (
     <div className='overlay' onMouseDown={(event) => artworkClose(event)}>
